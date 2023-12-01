ad13f3cd714ddd7b62c78d1a8d647edef1d593d8
CoreyBabs <coreymbabcock@gmail.com>
2023-11-30 23:43:19 -0700

    Add day 1 part 1 solution

---
+++ day-01/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    let input = input.split("\n").collect::<Vec<&str>>();
+    part1(input);
+    // println!("{}", input[0]);
+}
+
+fn part1(input: Vec<&str>) {
+    let digits: Vec<Vec<char>> = input.iter()
+    .map(|l| l.chars().filter(|c| c.is_digit(10)).collect())
+    .collect();
+    let mut sum = 0;
+    for digit in digits {
+        if digit.len() == 0 {
+            continue;
+        }
+
+        let mut amount = "".to_string();
+        if digit.len() == 1 {
+            amount.push(digit[0]);
+            amount.push(digit[0]);
+        }
+        else {
+            amount.push(*digit.first().unwrap());
+            amount.push(*digit.last().unwrap());
+        }
+
+        sum += amount.parse::<u32>().unwrap();
+    }
+
+    println!("{}", sum);
+}
