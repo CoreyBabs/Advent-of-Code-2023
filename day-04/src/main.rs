3884a022a904a41973312aaf983d6b9234a7c352
CoreyBabs <coreymbabcock@gmail.com>
2023-12-04 00:46:53 -0700

    Add day 4 part 2 solution
    Also slightly refactors part 1 solution to work better for part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     let mut input = input.split("\n").collect::<Vec<&str>>();
     input.pop();
     part1(&input);
+    part2(&input);
 }
 
 fn line_to_vec(line: &str) -> Vec<&str> {
@@ -11,12 +12,14 @@ fn line_to_vec(line: &str) -> Vec<&str> {
     split[1].split(" | ").collect()
 }
 
-fn vec_to_score(numbers: &Vec<&str>) -> usize {
+fn vec_to_count(numbers: &Vec<&str>) -> usize {
     let winning: Vec<&str> = numbers[0].split(" ").collect();
     let nums: Vec<&str> = numbers[1].split(" ").collect();
     
-    let amount: usize = nums.iter().filter(|n| !n.is_empty() && winning.contains(n)).count();
+    nums.iter().filter(|n| !n.is_empty() && winning.contains(n)).count()
+}
 
+fn count_to_score(amount: usize) -> usize {
     if amount == 0 {
         return 0;
     }
@@ -27,8 +30,30 @@ fn vec_to_score(numbers: &Vec<&str>) -> usize {
 fn part1(input: &Vec<&str>) {
     let sum: usize = input.iter()
         .map(|l| line_to_vec(l))
-        .map(|l| vec_to_score(&l))
+        .map(|l| count_to_score(vec_to_count(&l)))
         .sum();
 
+    println!("{}", sum);
+}
+
+fn part2(input: &Vec<&str>) {
+    let mut cards: Vec<u32> = vec![1; input.len()];
+
+    let amounts: Vec<usize> = input.iter()
+        .map(|l| line_to_vec(l))
+        .map(|l| vec_to_count(&l)).collect();
+
+    for (i, amount) in amounts.iter().enumerate() {
+        if amount == &0 {
+            continue;
+        }
+
+        let before = cards.clone();
+        for j in i+1..i+amount+1 {
+            cards[j] += before[i];
+        } 
+    }
+
+    let sum: u32 = cards.iter().sum();
     println!("{}", sum);
 }
