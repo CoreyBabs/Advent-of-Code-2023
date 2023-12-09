d21bfed75211bce95bd7801b228319f060409ab8
CoreyBabs <coreymbabcock@gmail.com>
2023-12-09 14:02:45 -0700

    Add solution for day 5 part 2

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,7 +1,10 @@
+use std::vec;
+
 fn main() {
     let input = include_str!("../input.txt");
     let input = input.split("\n\n").collect::<Vec<&str>>();
     part1(&input);
+    part2(&input);
 }
 
 fn part1(input: &Vec<&str>) {
@@ -41,31 +44,73 @@ fn part1(input: &Vec<&str>) {
     println!("{min}");
 }
 
-fn update_seeds(seeds: &mut Vec<Seed>, map: &Vec<Map>, compartment: &Compartment) {
-    println!("{:?}", compartment);
-    for seed in seeds {
-        let src = match compartment {
-            Compartment::Soil => seed.number,
-            Compartment::Fertilizer => seed.soil,
-            Compartment::Water => seed.fertilizer,
-            Compartment::Light => seed.water,
-            Compartment::Temp => seed.light,
-            Compartment::Humidity => seed.temperature,
-            Compartment::Location => seed.humidity,
-        };
+fn part2(input: &Vec<&str>) {
+    let seeds_pairs = input[0].split(": ").collect::<Vec<&str>>();
+    let seeds_pairs: Vec<&str> = seeds_pairs[1].split(" ").collect();
 
-        let mut dst = src;
-        let matching_range: Vec<&Map> = map.iter()
-            .filter(|m| m.src <= src && m.src + m.length - 1 >= src)
-            .collect();
+    let mut idx = 0;
+    let mut mins: Vec<usize> = vec![];
+    while idx < seeds_pairs.len() {
+        let mut seeds: Vec<usize> = 
+            construct_seed_list_from_pairs(seeds_pairs[idx], seeds_pairs[idx + 1]);
         
-        if matching_range.len() > 1 {
-            panic!("Found more than one mapped value.");
+        for (i, lines) in input.iter().enumerate() {
+            if i == 0 {
+                continue;
+            }
+
+            let map = construct_map(lines);
+            // println!("{}, {:?}", lines, &compartments[i-1]);
+           update_seed_values_in_place(&mut seeds, &map);
         }
 
-        if matching_range.len() == 1 {
-            let diff = src - matching_range[0].src;
-            dst = matching_range[0].dst + diff;
+
+        let min = seeds.iter()
+            .map(|s| s)
+            .min().unwrap();
+
+        mins.push(*min);
+        idx += 2;
+    }
+
+    let min = mins.iter().min().unwrap();
+
+    // println!("{:?}", seeds);
+    println!("{min}");
+}
+
+fn construct_seed_list_from_pairs(start: &str, length: &str) -> Vec<usize> {
+    let start: usize = start.parse().unwrap();
+    let length: usize = length.parse().unwrap();
+    
+    (start..start+length).collect()
+}
+
+fn update_seeds(seeds: &mut Vec<Seed>, map: &Vec<Map>, compartment: &Compartment) {
+    println!("{:?}", compartment);
+    let mut current_map: Option<&Map> = None;
+    seeds.iter_mut().for_each(|seed| {
+        let src = match compartment {
+            Compartment::Soil => &seed.number,
+            Compartment::Fertilizer => &seed.soil,
+            Compartment::Water => &seed.fertilizer,
+            Compartment::Light => &seed.water,
+            Compartment::Temp => &seed.light,
+            Compartment::Humidity => &seed.temperature,
+            Compartment::Location => &seed.humidity,
+        };
+
+        let mut dst = *src;
+
+        if current_map.is_none() || 
+            current_map.is_some_and(|m| src < &m.src || src > &(&m.src + &m.length - 1)) {
+            current_map = map.iter()
+                .find(|m| m.src <= *src && m.src + m.length - 1 >= *src);
+        }
+
+        match current_map {
+            Some(mr) => dst = mr.dst + (src - mr.src),
+            None => (), 
         }
 
         match compartment {
@@ -77,6 +122,27 @@ fn update_seeds(seeds: &mut Vec<Seed>, map: &Vec<Map>, compartment: &Compartment
             Compartment::Humidity => seed.humidity = dst,
             Compartment::Location => seed.location = dst,
         }
+    });
+}
+
+fn update_seed_values_in_place(seeds: &mut Vec<usize>, map: &Vec<Map>) {
+    let mut current_map: Option<&Map> = None;
+    for i in 0..seeds.len() {
+        let src = seeds[i];
+        let mut dst = src;
+
+        if current_map.is_none() || 
+            current_map.is_some_and(|m| src < m.src || src > m.src + m.length - 1) {
+            current_map = map.iter()
+                .find(|m| m.src <= src && m.src + m.length - 1 >= src);
+        }
+
+        match current_map {
+            Some(mr) => dst = mr.dst + (src - mr.src),
+            None => (), 
+        }
+
+        seeds[i] = dst;
     }
 }
 
@@ -86,7 +152,7 @@ fn construct_map(lines: &str) -> Vec<Map> {
         .map(|s| s.split(" ").collect::<Vec<&str>>())
         .collect();
 
-    let mut map: Vec<Map> = vec![];
+    let mut map: Vec<Map> = Vec::with_capacity(lines.len());
 
     for (i, value) in values.iter().enumerate() {
         if i == 0 || value.len() != 3 {
