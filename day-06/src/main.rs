b1b98c59bcaffd7633539590b7b4440db5432b6c
CoreyBabs <coreymbabcock@gmail.com>
2023-12-09 14:35:42 -0700

    Add solution for day 6 part 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     let mut input: Vec<&str> = input.split("\n").collect();
     input.pop();
     part1(&input);
+    part2(&input);
 }
 
 fn part1(input: &Vec<&str>) {
@@ -11,18 +12,32 @@ fn part1(input: &Vec<&str>) {
     let times: Vec<&str> = times[1].split_ascii_whitespace().collect();
     let dists: Vec<&str> = dists[1].split_ascii_whitespace().collect();
     
-    let times: Vec<u32> = times.iter().map(|t| t.parse().unwrap()).collect();
-    let dists: Vec<u32> = dists.iter().map(|t| t.parse().unwrap()).collect();
+    let times: Vec<usize> = times.iter().map(|t| t.parse().unwrap()).collect();
+    let dists: Vec<usize> = dists.iter().map(|t| t.parse().unwrap()).collect();
 
-    let counts: Vec<u32> = times.iter().zip(dists.iter())
+    let counts: Vec<usize> = times.iter().zip(dists.iter())
         .map(|(t,d)| get_count(t, d)).collect();
 
-    let prod: u32 = counts.iter().product();
+    let prod: usize = counts.iter().product();
 
     println!("{:?}", prod);
 }
 
-fn get_count(time: &u32, dist: &u32) -> u32 {
+fn part2(input: &Vec<&str>) {
+    let times: Vec<&str> = input[0].split(":").collect();
+    let dists: Vec<&str> = input[1].split(":").collect();
+    let times: Vec<&str> = times[1].split_ascii_whitespace().collect();
+    let dists: Vec<&str> = dists[1].split_ascii_whitespace().collect();
+    
+    let time: usize = times.join("").parse().unwrap();
+    let dist: usize = dists.join("").parse().unwrap();
+
+    let count = get_count(&time, &dist);
+
+    println!("{:?}", count);
+}
+
+fn get_count(time: &usize, dist: &usize) -> usize {
     let mut count = 0;
     for i in 0..time+1 {
         let time_diff = time - i;
