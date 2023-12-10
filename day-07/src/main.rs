165d992a0da17a969c83d1e79bd3293ea7863031
CoreyBabs <coreymbabcock@gmail.com>
2023-12-10 15:18:17 -0700

    Add solution for day 7 part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let mut input: Vec<&str> = input.split("\n").collect();
     input.pop();
     part1(&input);
+    part2(&input);
 }
 
 fn part1(input: &Vec<&str>) {
@@ -24,6 +25,26 @@ fn part1(input: &Vec<&str>) {
     println!("{:?}", sum);
 }
 
+fn part2(input: &Vec<&str>) {
+    let mut hands: Vec<Hand> = input.iter().map(|l| {
+        let mut hand = Hand::new(l);
+        let original = hand.hand.clone();
+        hand.hand = Hand::generate_strongest_hand(&hand.hand);
+        hand.calculate_strength();
+        hand.hand = original;
+        hand
+    })
+        .collect();
+    hands.sort_by(compare_hands_with_joker);
+
+    let mut sum = 0; 
+    for (i, hand) in hands.iter().enumerate() {
+        sum += hand.bid as usize * (i + 1);
+    }
+
+    println!("{:?}", sum);
+}
+
 fn compare_hands(l: &Hand, r: &Hand) -> Ordering {
     if l.stength != r.stength {
         return l.stength.cmp(&r.stength);
@@ -32,7 +53,7 @@ fn compare_hands(l: &Hand, r: &Hand) -> Ordering {
     let mut idx = 0;
     let mut lc = l.hand[idx];
     let mut rc = r.hand[idx];
-    while lc == rc {
+    while lc == rc && idx < l.hand.len() - 1 {
         idx += 1;
         lc = l.hand[idx];
         rc = r.hand[idx];
@@ -56,6 +77,85 @@ fn royal_to_number(royal: &char) -> &str {
     }
 }
 
+fn compare_hands_with_joker(l: &Hand, r: &Hand) -> Ordering {
+    if l.stength != r.stength {
+        return l.stength.cmp(&r.stength);
+    }
+
+    let mut idx = 0;
+    let mut lc = l.hand[idx];
+    let mut rc = r.hand[idx];
+    while lc == rc && idx < l.hand.len() - 1 {
+        idx += 1;
+        lc = l.hand[idx];
+        rc = r.hand[idx];
+    }
+
+    if lc.is_alphabetic() && rc.is_alphabetic() {
+        return royal_to_number_with_joker(&lc).cmp(&royal_to_number_with_joker(&rc));
+    }
+
+    let ls: usize;
+    let rs: usize;
+    
+    if lc.is_alphabetic() {
+        ls = royal_to_number_with_joker(&lc).parse().unwrap();
+    }
+    else {
+        ls = lc.to_digit(10).unwrap() as usize;
+    }
+
+    if rc.is_alphabetic() {
+        rs = royal_to_number_with_joker(&rc).parse().unwrap();
+    }
+    else {
+        rs = rc.to_digit(10).unwrap() as usize;
+    }
+
+    ls.cmp(&rs)
+}
+
+fn royal_to_number_with_joker(royal: &char) -> &str {
+    match royal {
+        'A' => "14",
+        'K' => "13",
+        'Q' => "12",
+        'J' => "0",
+        'T' => "10",
+        _ => panic!("Invalid royaly card"),
+    }
+}
+
+
+fn generate_possible_hands(hand: &Vec<char>, index: usize) -> Vec<Vec<char>> {
+    let possible = vec![
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        'T',
+        'J',
+        'Q',
+        'K',
+        'A'
+    ];
+
+    let mut hands = vec![];
+
+    for p in possible {
+        let mut new = hand.clone();
+        new[index] = p;
+       hands.push(new); 
+    }
+
+    hands
+}
+
 
 #[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
 struct Hand {
@@ -72,10 +172,6 @@ impl Hand {
         let bid: u32 = line[1].parse().unwrap();
         Hand { hand, bid, rank: 0, stength: 0 }
     } 
-
-    fn calculate_winnings(&self) -> u32 {
-        self.bid * self.rank 
-    }
 
     fn calculate_strength(&mut self) {
         let mut map: HashMap<char, u8> = HashMap::new();
@@ -110,5 +206,46 @@ impl Hand {
             _ => panic!("Invalid number of cards in hand")
             
         }
+    }
+
+    fn generate_strongest_hand(hand: &Vec<char>) -> Vec<char> {
+        if !hand.contains(&'J') {
+            return hand.clone();
+        }
+
+
+        let mut indexes = vec![];
+        for (i, card) in hand.iter().enumerate() {
+            if card == &'J' {
+                indexes.push(i);
+            }
+        }
+
+        let mut all_hands = generate_possible_hands(&hand, indexes[0]);
+        for (i, idx) in indexes.iter().enumerate() {
+            if i == 0 {
+                continue;
+            }
+
+            let mut new_hands = vec![];
+            for hand in all_hands.iter() {
+                new_hands.append(&mut generate_possible_hands(&hand, *idx));
+            }
+
+            all_hands = new_hands;
+        }
+
+        let mut all_hands: Vec<Hand> = all_hands.iter()
+            .map(|h| {
+                let mut hand = Hand { hand: h.to_vec(), bid: 0, rank: 0, stength: 0 };
+                hand.calculate_strength();
+                hand
+            })
+            .collect();
+
+        
+        all_hands.sort_by(compare_hands_with_joker);
+
+        all_hands.last().unwrap().hand.clone()
     }
 }
