8d1e89ff5d08f3c01d4a5ccd16cce435a01b44c9
CoreyBabs <coreymbabcock@gmail.com>
2023-12-22 01:47:04 -0700

    Add day 9 part 1 solution

---
+++ day-09/src/main.rs
@@ -0,0 +1,64 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    let mut input: Vec<&str> = input.split("\n").collect();
+    input.pop();
+
+    part1(&input);
+}
+
+fn part1(input: &Vec<&str>) {
+    let input: Vec<Vec<&str>> = input.iter()
+        .map(|l| l.split(" ").collect::<Vec<&str>>()).collect();
+
+    let input: Vec<Vec<i64>> = input.iter()
+        .map(|l| l.iter()
+            .map(|i| i.parse::<i64>()
+                .expect("Not an i64"))
+            .collect())
+        .collect();
+    
+    let mut total = 0;
+    for i in input {
+        let diffs = get_diffs(&i);
+        let diffs = update_diffs_with_preds(diffs);
+        total += diffs[0].last().unwrap();
+    }
+
+    println!("{:?}", total);
+}
+
+fn get_diffs(input: &Vec<i64>) -> Vec<Vec<i64>> {
+    let mut diffs: Vec<Vec<i64>> = vec![];
+    diffs.push(input.clone());
+    while !diffs.last().unwrap_or(&vec![]).iter().all(|d| d == &0) {
+        let current = diffs.last().unwrap();
+
+        let mut diff: Vec<i64> = Vec::with_capacity(input.len() - 1);
+        for (i, val) in current.iter().enumerate() {
+            if i == current.len() - 1 {
+                break;
+            }
+
+            diff.push(current[i + 1] - val);
+        }
+
+        // println!("{:?}", diff);
+        diffs.push(diff)
+    }
+
+    // println!("{:?}", diffs);
+    diffs
+}
+
+fn update_diffs_with_preds(mut diffs: Vec<Vec<i64>>) -> Vec<Vec<i64>> {
+    diffs.last_mut().unwrap().push(0);
+    for i in (0..diffs.len() - 1).rev() {
+        let prev = &diffs[i+1].clone();
+        let prev = prev.last().unwrap();
+        let current = &mut diffs[i];
+        let pred = current.last().unwrap();
+        current.push(prev + pred);
+    }
+
+    diffs
+}
