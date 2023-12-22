5c76ca5ad4f555fbc95c2b9b42cff5f4b422a3be
CoreyBabs <coreymbabcock@gmail.com>
2023-12-21 17:42:48 -0700

    Add non working day 8 part 2 solution.
    The solution is theoretically correct, but the runtime is way too slow

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let input = include_str!("../input.txt");
     let input: Vec<&str> = input.split("\n\n").collect();
     part1(&input);
+    part2(&input);
 }
 
 fn part1(input: &Vec<&str>) {
@@ -31,7 +32,52 @@ fn part1(input: &Vec<&str>) {
         }
     }
 
+    println!("{:?}", count);
+}
+
+fn part2(input: &Vec<&str>) {
+    let directions: Vec<char> = input[0].chars().collect();
+    let map = construct_map(input[1]);
+
+    let mut keys: Vec<String> = map.clone().into_keys().filter(|k| k.ends_with("A")).collect();
+
+    println!("{:?}", keys);
+    // println!("{:?}", map);
     
+    let mut found = false;
+    let mut count: usize = 0;
+    while !found && count < 16342438708751 {
+        for dir in directions.iter() {
+            // println!("{:?}", keys);
+            count += 1;
+            let mut new_keys: Vec<String> = Vec::with_capacity(keys.len());
+            for k in keys.iter() {
+                let mut key: &str = "";
+                if dir == &'R' {
+                    key = &map.get(k).unwrap().1;
+                }
+                else if dir == &'L' {
+                    key = &map.get(k).unwrap().0;
+                }
+
+                new_keys.push(key.to_string());
+            };
+
+            if new_keys.iter().any(|k| !k.ends_with("Z")) {
+                // let zc = new_keys.iter().filter(|z| z.ends_with("Z")).count();
+                // println!("{:?}", new_keys);
+                keys = new_keys; 
+            }
+            else {
+                found = true;
+            }
+
+            if count == 16342438708751 {
+                println!("{:?}", keys);
+            }
+        }
+    }
+
     println!("{:?}", count);
 }
 
