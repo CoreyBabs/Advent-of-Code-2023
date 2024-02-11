6feb81a475bced07e365d89618a2383d3aaf831b
CoreyBabs <coreymbabcock@gmail.com>
2024-02-10 17:06:38 -0700

    Add elixir day 1 part 2 solution

--- elixir/day-01/main.exs
+++ elixir/day-01/main.exs
@@ -32,6 +32,40 @@ defmodule Day1 do
     Integer.parse(combined)
   end
 
+  defp get_indices_of_numbers(line, indices, digits, total_digits) when length(digits) === 1 do
+    head = hd(digits)
+    v = total_digits[head]
+
+    {_, rhead} = Regex.compile(head)
+    words = Regex.scan(rhead, line, return: :index)
+    words = Enum.map(words, fn t -> {elem(List.first(t), 0), v} end)
+    indices = indices ++ words
+
+    {_, rv} = Regex.compile(v)
+    nums = Regex.scan(rv, line, return: :index)
+    nums = Enum.map(nums, fn t -> {elem(List.first(t), 0), v} end)
+    indices = indices ++ nums
+    
+    Enum.sort_by(indices, fn {value, _} -> value end)
+  end
+
+  defp get_indices_of_numbers(line, indices, digits, total_digits) do
+    [head | tail] = digits
+    v = total_digits[head]
+
+    {_, rhead} = Regex.compile(head)
+    words = Regex.scan(rhead, line, return: :index)
+    words = Enum.map(words, fn t -> {elem(List.first(t), 0), v} end)
+    indices = indices ++ words
+
+    {_, rv} = Regex.compile(v)
+    nums = Regex.scan(rv, line, return: :index)
+    nums = Enum.map(nums, fn t -> {elem(List.first(t), 0), v} end)
+    indices = indices ++ nums
+
+    get_indices_of_numbers(line, indices, tail, total_digits)
+  end
+
   def part1(input, sum) when length(input) > 1 do
     [head | tail ] = input
     {a, _} = get_digit(head, {}) |> combine_tuple()
@@ -45,10 +79,50 @@ defmodule Day1 do
     sum = sum + a
     IO.puts(sum)
   end
+
+  def part2(input, sum, total_digits) when length(input) > 1 do
+    [head | tail] = input
+    keys = Map.keys(total_digits)
+    idxs = get_indices_of_numbers(head, [], keys, total_digits)
+    first = List.first(idxs)
+    last = List.last(idxs)
+    tuple = {elem(first, 1), elem(last, 1)}
+
+    {a, _} = combine_tuple(tuple)
+    sum = sum + a
+    part2(tail, sum, total_digits)
+  end
+
+  def part2(input, sum, total_digits) do
+    head = hd(input)
+    keys = Map.keys(total_digits)
+
+    idxs = get_indices_of_numbers(head, [], keys, total_digits)
+    first = List.first(idxs)
+    last = List.last(idxs)
+    tuple = {elem(first, 1), elem(last, 1)}
+
+    {a, _} = combine_tuple(tuple)
+    sum = sum + a
+    IO.puts(sum)
+  end
 end
+
+  digit_chars = %{
+    "one" => "1",
+    "two" => "2",
+    "three" => "3",
+    "four" => "4",
+    "five" => "5",
+    "six" => "6",
+    "seven" => "7",
+    "eight" => "8",
+    "nine" => "9",
+  }
 
 file = File.read!("./input.txt")
 file = String.trim(file)
 file = String.split(file, "\n")
 
 Day1.part1(file, 0)
+Day1.part2(file, 0, digit_chars)
